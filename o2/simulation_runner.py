8c203a6f7aed9ba8ee5f632f1ca1af66e9908f7f
Jannis Rosenbaum <j.ro@dock51.de>
2024-08-22 17:23:37 +0300

    remove dynamik dependency

--- o2/simulation_runner.py
+++ o2/simulation_runner.py
@@ -1,21 +1,12 @@
 import traceback
 from datetime import datetime
-from typing import TYPE_CHECKING, Collection, TypeAlias
+from typing import TYPE_CHECKING, TypeAlias
 
-import pandas as pd
 from bpdfr_simulation_engine.simulation_engine import run_simpy_simulation
 from bpdfr_simulation_engine.simulation_stats_calculator import (
     KPIMap,
     LogInfo,
     ResourceKPI,
-)
-from dynamik.model import Event
-from wta import (
-    WAITING_TIME_BATCHING_KEY,
-    WAITING_TIME_TOTAL_KEY,
-    EventLogIDs,
-    add_enabled_timestamp,
-    convert_timestamp_columns_to_datetime,
 )
 
 TaskKPIs: TypeAlias = dict[str, KPIMap]
@@ -24,55 +15,12 @@ SimulationKPIs: TypeAlias = tuple[KPIMap, TaskKPIs, RessourceKPIs, datetime, dat
 
 RunSimulationResult: TypeAlias = tuple[KPIMap, TaskKPIs, RessourceKPIs, LogInfo]
 
-Log: TypeAlias = Collection[Event]
 
 if TYPE_CHECKING:
     from o2.models.state import State
 
 
 class SimulationRunner:
-    @staticmethod
-    def parse_log(logStringIO):
-        # Override column names
-        log_ids = EventLogIDs(
-            case="case_id",
-            activity="activity",
-            start_time="start_time",
-            end_time="end_time",
-            enabled_time="enable_time",
-            resource="resource",
-        )
-        # Reset the StringIO to read from the beginning
-        logStringIO.seek(0)
-
-        log = pd.read_csv(logStringIO)
-        log = convert_timestamp_columns_to_datetime(log, log_ids, utc=True)
-
-        duration_columns = [WAITING_TIME_TOTAL_KEY, WAITING_TIME_BATCHING_KEY]
-        for column in duration_columns:
-            if column in log.columns:
-                log[column] = pd.to_timedelta(log[column])
-
-        # discarding unnecessary columns
-        log = log[
-            [
-                log_ids.case,
-                log_ids.activity,
-                log_ids.resource,
-                log_ids.start_time,
-                log_ids.end_time,
-            ]
-        ]
-
-        # NOTE: sorting by end time is important for concurrency oracle that is run during batching analysis
-        log.sort_values(
-            by=[log_ids.end_time, log_ids.start_time, log_ids.activity], inplace=True
-        )
-
-        add_enabled_timestamp(log, log_ids)
-
-        return log
-
     @staticmethod
     def run_simulation(
         state: "State",
