0ec2d633e19963b5211afa077264389ee9e95a61
Jannis Rosenbaum <j.ro@dock51.de>
2024-10-17 14:13:30 +0300

    add run simulation script

---
+++ run_simulation.sh
@@ -0,0 +1,6 @@
+diff_res_bpsim start-simulation \
+    --bpmn_path ./examples/demo_speed/model.bpmn \
+    --json_path ./examples/demo_speed/timetable.json \
+    --total_cases 20 \
+    --log_out_path ./examples/demo_speed/event_log.csv \
+    --stat_out_path ./examples/demo_speed/stats.csv
