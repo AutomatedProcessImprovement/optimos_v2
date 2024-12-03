db5fc54036a986d97ad51be5f4d1ccc0fdfc1e3f
Jannis Rosenbaum <j.ro@dock51.de>
2024-12-03 12:23:46 +0200

    Add ModifySizeRuleByDurationFnCostImpact Action

--- o2/actions/new_actions/modify_size_rule_by_duration_fn.py
+++ o2/actions/new_actions/modify_size_rule_by_duration_fn.py
@@ -62,3 +62,68 @@ class ModifyBatchSizeIfNoDurationImprovement(ModifySizeRuleBaseAction):
             )
 
 
+class ModifySizeRuleByDurationFnCostImpact(ModifySizeRuleBaseAction):
+    """An Action to modify size batching rules based on the duration fn.
+
+    If batch size increment reduces Duration => Increase Batch Size
+    - Sorted by the cost impact of that size increment.
+    """
+
+    params: ModifySizeRuleByDurationFnParamsType
+
+    @staticmethod
+    def rate_self(store: "Store", input: SelfRatingInput) -> RateSelfReturnType:
+        """Generate a best set of parameters & self-evaluates this action."""
+        timetable = store.current_timetable
+        constraints = store.constraints
+        state = store.current_state
+
+        task_ids = timetable.get_task_ids()
+        duration_impacts: dict["RuleSelector", float] = {}
+
+        for task_id in task_ids:
+            firing_rule_selectors = timetable.get_firing_rule_selectors_for_task(
+                task_id, rule_type=RULE_TYPE.SIZE
+            )
+            if not firing_rule_selectors:
+                continue
+
+            size_constraints = constraints.get_batching_size_rule_constraints(task_id)
+            if size_constraints is None:
+                continue
+            size_constraint = size_constraints[0]
+            for firing_rule_selector in firing_rule_selectors:
+                firing_rule = firing_rule_selector.get_firing_rule_from_state(state)
+                if firing_rule is None:
+                    continue
+                size = firing_rule.value
+
+                old_duration = size_constraint.duration_fn_lambda(size)
+                new_duration = size_constraint.duration_fn_lambda(size + 1)
+
+                old_cost = size_constraint.cost_fn_lambda(size)
+                new_cost = size_constraint.cost_fn_lambda(size + 1)
+                if new_duration < old_duration:
+                    duration_impacts[firing_rule_selector] = old_cost - new_cost
+
+        sorted_duration_impacts = sorted(
+            duration_impacts.keys(),
+            key=lambda firing_rule_selector: duration_impacts[firing_rule_selector],
+            reverse=True,
+        )
+
+        for rule_selector in sorted_duration_impacts:
+            size_constraint = constraints.get_batching_size_rule_constraints(
+                rule_selector.batching_rule_task_id
+            )[0]
+
+            yield (
+                ModifySizeRuleBaseAction.get_default_rating(),
+                ModifySizeRuleByDurationFnCostImpact(
+                    ModifySizeRuleByDurationFnParamsType(
+                        rule=rule_selector,
+                        size_increment=1,
+                        duration_fn=size_constraint.duration_fn,
+                    )
+                ),
+            )
