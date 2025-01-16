51b9515f6cb984c313048ff2f9f195d38e9d34e8
Jannis Rosenbaum <j.ro@dock51.de>
2025-01-16 13:36:57 +0200

    add unit test for size batching rule, to ensure prosimos is working correctly

--- tests/simulation_runner_test.py
+++ tests/simulation_runner_test.py
@@ -23,3 +23,52 @@ def test_simulation_runner(simple_state: State):
     assert evaluation.total_duration > 0
     assert evaluation.total_cost > 0
     assert evaluation.total_waiting_time > 0
+
+def test_size_batching_rule(two_tasks_state: State):
+    """This is mostly a regression test for a bug in the batching engine."""
+    rule = TimetableGenerator.batching_size_rule(
+        TimetableGenerator.SECOND_ACTIVITY,
+        2,
+        1.0,
+        comparator=COMPARATOR.GREATER_THEN_OR_EQUAL,
+    )
+    state = two_tasks_state.replace_timetable(
+        batch_processing=[rule],
+        total_cases=2,
+        arrival_time_distribution=TimetableGenerator.arrival_time_distribution(
+            # Event every 5 minutes
+            min=5 * 60,
+            max=5 * 60,
+        ),
+    )
+
+    global_kpis, task_kpis, resource_kpis, log_info = SimulationRunner.run_simulation(
+        state, show_simulation_errors=True
+    )
+
+    case_0 = log_info.trace_list[0]
+    case_1 = log_info.trace_list[1]
+
+    assert case_0.event_list[0].task_id == TimetableGenerator.FIRST_ACTIVITY
+    assert case_0.event_list[1].task_id == TimetableGenerator.SECOND_ACTIVITY
+    assert case_1.event_list[0].task_id == TimetableGenerator.FIRST_ACTIVITY
+    assert case_1.event_list[1].task_id == TimetableGenerator.SECOND_ACTIVITY
+
+    # Enabled directly
+    assert case_0.event_list[0].enabled_at == 0 * 60
+    # Enabled after waiting for the first task to complete
+    assert case_0.event_list[1].enabled_at == 60 * 60
+    # Enabled after second arrival (5 minutes after the first case)
+    assert case_1.event_list[0].enabled_at == 5 * 60
+    # Enabled after waiting for the first task (of snd case) to complete
+    assert case_1.event_list[1].enabled_at == 120 * 60
+
+    # Completed directly after arrival
+    assert case_0.event_list[0].completed_at == 60 * 60
+    # Completed after processing the the first task
+    assert case_1.event_list[0].completed_at == 2 * 60 * 60
+    # Completed after processing waiting for the first task of both cases
+    # (2h processing time, because no batching bonus)
+    assert case_0.event_list[1].completed_at == 4 * 60 * 60
+    assert case_1.event_list[1].completed_at == 4 * 60 * 60
+
