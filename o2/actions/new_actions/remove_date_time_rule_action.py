36c1e6c0d742e1ff1bcf06adb6f3fd4b22c5be50
Jannis Rosenbaum <j.ro@dock51.de>
2025-01-21 09:56:29 +0200

    add RemoveDateTimeRule action

---
+++ o2/actions/new_actions/remove_date_time_rule_action.py
@@ -0,0 +1,90 @@
+from dataclasses import replace
+
+from o2.actions.base_actions.base_action import BaseAction, BaseActionParamsType
+from o2.models.days import DAY
+from o2.models.self_rating import SelfRatingInput
+from o2.models.state import State, TabuState
+from o2.store import Store
+
+
+class RemoveDateTimeRuleActionParamsType(BaseActionParamsType):
+    """Parameter for RemoveDateTimeRuleAction."""
+
+    task_id: str
+    day: DAY
+
+
+class RemoveDateTimeRuleAction(BaseAction):
+    """RemoveDateTimeRuleAction will remove a day of week and time of day rule."""
+
+    params: RemoveDateTimeRuleActionParamsType
+
+    def apply(self, state: State, enable_prints: bool = True) -> State:
+        """Create a copy of the timetable with the rule removed."""
+        timetable = state.timetable
+        task_id = self.params["task_id"]
+
+        best_selector = timetable.get_longest_time_period_for_daily_hour_firing_rules(
+            task_id, self.params["day"]
+        )
+
+        if best_selector is None:
+            # TODO: Here we should add a new rule
+            return TabuState()
+
+        day_selector, lower_bound_selector, upper_bound_selector = best_selector
+
+        index, batching_rule = timetable.get_batching_rule(lower_bound_selector)
+        assert batching_rule is not None and index is not None
+
+        day_selector_index = (
+            day_selector.firing_rule_index[1]
+            if day_selector is not None and day_selector.firing_rule_index is not None
+            else -1
+        )
+        lower_bound_index = (
+            lower_bound_selector.firing_rule_index[1]
+            if lower_bound_selector.firing_rule_index is not None
+            else -1
+        )
+        upper_bound_index = (
+            upper_bound_selector.firing_rule_index[1]
+            if upper_bound_selector.firing_rule_index is not None
+            else -1
+        )
+        assert upper_bound_selector.firing_rule_index is not None
+        or_rules_index = upper_bound_selector.firing_rule_index[0]
+        new_batching_rule = replace(
+            batching_rule,
+            firing_rules=batching_rule.firing_rules[:or_rules_index]
+            + [
+                [
+                    and_rule
+                    for i, and_rule in enumerate(
+                        batching_rule.firing_rules[or_rules_index]
+                    )
+                    if i
+                    not in [
+                        day_selector_index,
+                        lower_bound_index,
+                        upper_bound_index,
+                    ]
+                ]
+            ]
+            + batching_rule.firing_rules[or_rules_index + 1 :],
+        )
+        if len(new_batching_rule.firing_rules) == 0:
+            return state.replace_timetable(
+                batch_processing=timetable.batch_processing[:index]
+                + timetable.batch_processing[index + 1 :],
+            )
+
+        return state.replace_timetable(
+            batch_processing=timetable.batch_processing[:index]
+            + [new_batching_rule]
+            + timetable.batch_processing[index + 1 :],
+        )
+
+    @staticmethod
+    def rate_self(store: Store, input: SelfRatingInput):
+        raise NotImplementedError("Not implemented")
