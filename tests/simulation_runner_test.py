fcc50b7d22aa4352629c13e6817d273f3bc5f46b
Jannis Rosenbaum <j.ro@dock51.de>
2025-01-27 15:50:12 +0200

    add sanity-check unit test for week day batching rule

--- tests/simulation_runner_test.py
+++ tests/simulation_runner_test.py
@@ -151,3 +151,38 @@ def test_ready_wt_batching_rule(two_tasks_state: State):
     assert case_0.event_list[1].completed_at == 11 * 60 * 60
     assert case_1.event_list[1].completed_at == 11 * 60 * 60
     assert case_2.event_list[1].completed_at == 11 * 60 * 60
+
+
+def test_week_day_batching_rule(two_tasks_state: State):
+    """This is mostly a regression test for a bug in the batching engine."""
+    Settings.SHOW_SIMULATION_ERRORS = True
+    Settings.RAISE_SIMULATION_ERRORS = True
+    rule = TimetableGenerator.week_day_rule(
+        TimetableGenerator.SECOND_ACTIVITY,
+        DAY.FRIDAY,
+    )
+    state = two_tasks_state.replace_timetable(
+        batch_processing=[rule],
+        total_cases=2,
+        arrival_time_distribution=TimetableGenerator.arrival_time_distribution(
+            min=5 * 60, max=5 * 60
+        ),
+    )
+    global_kpis, task_kpis, resource_kpis, log_info = SimulationRunner.run_simulation(
+        state
+    )
+
+    case_0 = log_info.trace_list[0]
+    case_1 = log_info.trace_list[1]
+    assert case_0.event_list[0].enabled_at == 0 * 60
+    assert case_1.event_list[0].enabled_at == 5 * 60
+    assert case_0.event_list[1].enabled_at == 1 * 60 * 60
+    assert case_1.event_list[1].enabled_at == 2 * 60 * 60
+
+    assert case_0.event_list[0].completed_at == 1 * 60 * 60
+    assert case_1.event_list[0].completed_at == 2 * 60 * 60
+    # 1 Hour of waiting + 4 days (till Friday) + 1 hour of processing
+    assert case_0.event_list[1].completed_at == (1 + 4 * 24 + 1) * 60 * 60
+    assert case_1.event_list[1].completed_at == (1 + 4 * 24 + 1) * 60 * 60
+
+
