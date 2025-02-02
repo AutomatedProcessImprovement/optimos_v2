6e0843d39093bb32bb6fb141cb7ca9cdb3952bee
Jannis Rosenbaum <j.ro@dock51.de>
2025-02-02 14:47:10 +0200

    major refatoring of ModifySizeRuleBaseAction

--- o2/actions/base_actions/modify_size_rule_base_action.py
+++ o2/actions/base_actions/modify_size_rule_base_action.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 
 from sympy import Symbol, lambdify
 
@@ -47,37 +47,25 @@ class ModifySizeRuleBaseAction(BatchingRuleBaseAction, ABC, str=False):
         timetable = state.timetable
         rule_selector = self.params["rule"]
 
-        (rule_index, old_rule) = timetable.get_batching_rule(rule_selector)
-        if old_rule is None or rule_index is None:
+        _, batching_rule = timetable.get_batching_rule(rule_selector)
+        if batching_rule is None:
             print(f"BatchingRule not found for {rule_selector}")
             return state
-        old_size = self.get_dominant_distribution(old_rule).key
-        new_size = int(old_size) + self.params["size_increment"]
-        fn = lambdify(Symbol("size"), self.params["duration_fn"])
 
-        if new_size < 1:
-            return TabuState()
+        firing_rule = batching_rule.get_firing_rule(rule_selector)
+        if firing_rule is None:
+            print(f"FiringRule not found for {rule_selector}")
+            return state
 
-        new_rule = BatchingRule.from_task_id(
-            task_id=old_rule.task_id,
-            size=new_size,
-            firing_rules=[
-                # TODO: Do not replace the whole firing rules,
-                # just the one that needs to be changed
-                FiringRule.gte(RULE_TYPE.SIZE, new_size),
-            ],
+        new_firing_rule = replace(
+            firing_rule, value=firing_rule.value + self.params["size_increment"]
         )
 
-        if enable_prints:
-            print(
-                f"\t\t>> Modifying rule {old_rule.id()} to new size = {old_size} -> {new_size} & duration_modifier = {fn(new_size)}"  # noqa: E501
-            )
-
-        return state.replace_timetable(
-            batch_processing=timetable.batch_processing[:rule_index]
-            + [new_rule]
-            + timetable.batch_processing[rule_index + 1 :],
+        new_timetable = state.timetable.replace_firing_rule(
+            rule_selector, new_firing_rule
         )
+
+        return replace(state, timetable=new_timetable)
 
     def get_dominant_distribution(self, old_rule: BatchingRule) -> Distribution:
         """Find the size distribution with the highest probability."""
