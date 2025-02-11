0dfa32cbb250ecec7737dc10233eb03ed0772c71
Jannis Rosenbaum <j.ro@dock51.de>
2025-02-11 13:08:55 +0200

    use new debug arg in run

--- run.sh
+++ run.sh
@@ -9,11 +9,13 @@
 module load any/python/3.8.3-conda
 conda activate opti2
 
-~/.conda/envs/opti2/bin/python ./o2_evaluation/data_collector.py \
+conda run -n opti2 python ./o2_evaluation/data_collector.py \
     --active-scenarios Purchasing \
     --models "Tabu Search" \
     --max-iterations 1500 \
     --dump-interval 250 \
     --max-non-improving-actions 1250 \
     --max-threads 24 \
+    --log-level DEBUG \
+    --log-file ./logs/optimos_v2_test_run.log \
     --log-to-tensor-board True
