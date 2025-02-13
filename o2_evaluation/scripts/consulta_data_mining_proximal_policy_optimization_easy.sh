459b4932f2ff1d588bae71cdb9112bcb7e713254
Jannis Rosenbaum <j.ro@dock51.de>
2025-02-14 00:16:20 +0200

    Add all planned evaluation scripts

---
+++ o2_evaluation/scripts/consulta_data_mining_proximal_policy_optimization_easy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name="Optimos V2 Run consulta_data_mining Proximal Policy Optimization EASY"
+#SBATCH --partition=main
+#SBATCH --time=02:00:00
+#SBATCH --mem=12G
+#SBATCH --cpus-per-task=25
+
+module load any/python/3.8.3-conda
+conda activate opti2
+
+conda run -n opti2 --no-capture-output python ./o2_evaluation/data_collector.py \
+    --active-scenarios "consulta_data_mining" \
+    --models "Proximal Policy Optimization" \
+    --number-of-cases 1000 \
+    --duration-fn "1/size" \
+    --max-batch-size 100 \
+    --max-iterations 1500 \
+    --dump-interval 250 \
+    --max-non-improving-actions 1250 \
+    --max-threads 24 \
+    --log-level DEBUG \
+    --log-file ./logs/consulta_data_mining_proximal_policy_optimization_easy.log \
+    --log-to-tensor-board True \
+    --archive-tensorboard-logs False
---
+++ o2_evaluation/scripts/loanorigination-mc-lu_proximal_policy_optimization_easy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name="Optimos V2 Run LoanOrigination-MC-LU Proximal Policy Optimization EASY"
+#SBATCH --partition=main
+#SBATCH --time=02:00:00
+#SBATCH --mem=12G
+#SBATCH --cpus-per-task=25
+
+module load any/python/3.8.3-conda
+conda activate opti2
+
+conda run -n opti2 --no-capture-output python ./o2_evaluation/data_collector.py \
+    --active-scenarios "LoanOrigination-MC-LU" \
+    --models "Proximal Policy Optimization" \
+    --number-of-cases 1000 \
+    --duration-fn "1/size" \
+    --max-batch-size 100 \
+    --max-iterations 1500 \
+    --dump-interval 250 \
+    --max-non-improving-actions 1250 \
+    --max-threads 24 \
+    --log-level DEBUG \
+    --log-file ./logs/loanorigination-mc-lu_proximal_policy_optimization_easy.log \
+    --log-to-tensor-board True \
+    --archive-tensorboard-logs False
