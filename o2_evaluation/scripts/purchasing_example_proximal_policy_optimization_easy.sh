f323affd05c1aee5eb596588c36e13ca085db0f4
Jannis Rosenbaum <j.ro@dock51.de>
2025-02-18 12:03:38 +0200

    Re-Generate scripts

---
+++ o2_evaluation/scripts/purchasing_example_proximal_policy_optimization_easy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name="Optimos V2 Run purchasing_example Proximal Policy Optimization EASY"
+#SBATCH --partition=main
+#SBATCH --time=027:00:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=1
+
+module load any/python/3.8.3-conda
+conda activate opti2
+
+conda run -n opti2 --no-capture-output python ./o2_evaluation/data_collector.py \
+    --name "purchasing_example_easy" \
+    --active-scenarios "purchasing_example" \
+    --models "Proximal Policy Optimization" \
+    --number-of-cases 1000 \
+    --duration-fn "1/size" \
+    --max-batch-size 100 \
+    --max-iterations 15000 \
+    --dump-interval 100 \
+    --max-non-improving-actions 50 \
+    --max-threads 24 \
+    --log-level DEBUG \
+    --log-file ./logs/proximal_policy_optimization_purchasing_example_easy.log \
+    --log-to-tensor-board \
+    --no-archive-tensorboard-logs
