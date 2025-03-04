fee667ac3c8911a0409dff87014d02404d23412e
Jannis Rosenbaum <j.ro@dock51.de>
2025-03-04 22:10:40 +0200

    Add new Scripts

---
+++ o2_evaluation/scripts/ac-crd_proximal_policy_optimization_hard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name="Optimos V2 Run AC-CRD Proximal Policy Optimization HARD"
+#SBATCH --partition=main
+#SBATCH --time=012:00:00
+#SBATCH --mem=12G
+#SBATCH --cpus-per-task=2
+
+module load any/python/3.8.3-conda
+conda activate opti2
+
+conda run -n opti2 --no-capture-output python ./o2_evaluation/data_collector.py \
+    --name "ac-crd_hard" \
+    --active-scenarios "AC-CRD" \
+    --agents "Proximal Policy Optimization" \
+    --number-of-cases 1000 \
+    --duration-fn "1 * size" \
+    --sa-cooling-factor auto \
+    --sa-initial-temperature auto \
+    --max-batch-size 50 \
+    --max-iterations 10001 \
+    --dump-interval 1000 \
+    --max-non-improving-actions 40 \
+    --max-threads 24 \
+    --max-number-of-actions-to-select 24 \
+    --log-level DEBUG \
+    --log-file ./logs/proximal_policy_optimization_ac-crd_hard.log \
+    --log-to-tensor-board \
+    --no-archive-tensorboard-logs
---
+++ o2_evaluation/scripts/poc_simulated_annealing_hard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name="Optimos V2 Run POC Simulated Annealing HARD"
+#SBATCH --partition=main
+#SBATCH --time=05:00:00
+#SBATCH --mem=60G
+#SBATCH --cpus-per-task=25
+
+module load any/python/3.8.3-conda
+conda activate opti2
+
+conda run -n opti2 --no-capture-output python ./o2_evaluation/data_collector.py \
+    --name "poc_hard" \
+    --active-scenarios "POC" \
+    --agents "Simulated Annealing" \
+    --number-of-cases 1000 \
+    --duration-fn "1 * size" \
+    --sa-cooling-factor auto \
+    --sa-initial-temperature auto \
+    --max-batch-size 50 \
+    --max-iterations 1001 \
+    --dump-interval 2500 \
+    --max-non-improving-actions 500 \
+    --max-threads 24 \
+    --max-number-of-actions-to-select 24 \
+    --log-level DEBUG \
+    --log-file ./logs/simulated_annealing_poc_hard.log \
+    --log-to-tensor-board \
+    --no-archive-tensorboard-logs
