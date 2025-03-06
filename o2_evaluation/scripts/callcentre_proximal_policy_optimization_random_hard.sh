8a435bb5eaded55b647b7d90c209f084b2320511
Jannis Rosenbaum <j.ro@dock51.de>
2025-03-06 02:32:23 +0200

    add new random ppo scripts

---
+++ o2_evaluation/scripts/callcentre_proximal_policy_optimization_random_hard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name="Optimos V2 Run callcentre Proximal Policy Optimization Random HARD"
+#SBATCH --partition=main
+#SBATCH --time=05:00:00
+#SBATCH --mem=60G
+#SBATCH --cpus-per-task=25
+
+module load any/python/3.8.3-conda
+conda activate opti2
+
+conda run -n opti2 --no-capture-output python ./o2_evaluation/data_collector.py \
+    --name "callcentre_hard" \
+    --active-scenarios "callcentre" \
+    --agents "Proximal Policy Optimization Random" \
+    --number-of-cases 1000 \
+    --duration-fn "1 * size" \
+    --sa-cooling-factor auto \
+    --sa-initial-temperature auto \
+    --max-batch-size 50 \
+    --max-iterations 1001 \
+    --dump-interval 2500 \
+    --max-non-improving-actions 500 \
+    --max-threads 24 \
+    --max-number-of-actions-to-select 24 \
+    --log-level DEBUG \
+    --log-file ./logs/proximal_policy_optimization_random_callcentre_hard.log \
+    --log-to-tensor-board \
+    --no-archive-tensorboard-logs
