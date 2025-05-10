b92821f86890a9aebe1836d65ada05a06d9de534
Jannis Rosenbaum <j.ro@dock51.de>
2025-05-11 02:06:13 +0300

    remove old PPO Env code

--- o2/ppo_utils/ppo_env.py
+++ o2/ppo_utils/ppo_env.py
@@ -50,57 +50,9 @@ class PPOEnv(Env[StateType, np.int64]):
         return self.state, {}
 
     def step(self, action: np.int64) -> tuple[StateType, float, bool, bool, dict]:
-        self.stepCount += 1
-
-        action_obj = self.actions[action]
-        print_l0(f"Step {self.stepCount} Iteration {self.iteration}")
-        print_l1(f"Action #{action}: {action_obj}")
-        if action_obj is None:
-            return self.state, 0, False, True, {}
-
-        reward: float = 0
-
-        try:
-            new_solution = Solution.from_parent(self.store.solution, action_obj)
-        except Exception as e:
-            error("Error in action:", e)
-            self.store.mark_action_as_tabu(action_obj)
-            return self.state, -1, False, False, {}
-        chosen_tries, not_chosen_tries = self.store.process_many_solutions([new_solution], None)
-
-        # TODO Improve scores based on how good/bad the solution is
-        if not_chosen_tries:
-            # TODO Make sure an invalid action / exception reaches this point
-            reward = -1
-        elif chosen_tries[0][0] == FRONT_STATUS.IN_FRONT:
-            reward = 1
-        elif chosen_tries[0][0] == FRONT_STATUS.IS_DOMINATED:
-            reward = 10
-        else:
-            reward = 0
-
-        done = False
-        truncated = False
-        info = {}
-
-        self.actions = PPOInput.get_actions_from_store(self.store)
-        self.action_space = PPOInput.get_action_space_from_actions(self.actions)
-        self.observation_space = PPOInput.get_observation_space(self.store)
-        self.state = PPOInput.get_state_from_store(self.store)
-
-        print_l1(f"Done. Reward: {reward}")
-        action_count = len([a for a in self.actions if a is not None])
-        if action_count == 0:
-            print_l1("No actions available for next step")
-            done = True
-        else:
-            print_l1(f"{action_count} actions available for next step")
-
-        if self.stepCount >= self.max_steps:
-            print_l1("Max steps reached")
-            done = True
-
-        return self.state, reward, done, truncated, info
+        # As we are reimplementing PPO in parts, the step function is actually not needed.
+        # So we raise an exception to avoid using it.
+        raise Exception("PPOEnv does not support step")
 
     def action_masks(self) -> np.ndarray:
         """Get the action mask for the current set of actions."""
